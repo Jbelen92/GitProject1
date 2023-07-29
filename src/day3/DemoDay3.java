ccdcfffbdc223da831c88eea52c74334a519d157
Jbelen92 <140672110+Jbelen92@users.noreply.github.com>
2023-07-29 11:11:45 -0400

    Update DemoDay3.java

--- src/day3/DemoDay3.java
+++ src/day3/DemoDay3.java
@@ -4,7 +4,7 @@ public class DemoDay3 {
 	
 	public static void main(String args []) {
 
-		String country = "USA"
+		String country = "USA";
 		String state = "VA";
 		System.out.println("Jorge");
 	}
